978bbe020c8e4c13a842398141b0538d19dcbbaf
guilhermesurek <guilhermesurek@gmail.com>
2020-03-15 12:50:08 -0300

    Setting login required to menu, upload and download view

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
+@login_required()
 def menu_view(request):
     context = {
         'menu_options': [
--- upload/views.py
+++ upload/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required()
 def upload_view(request):
     return HttpResponse('Upload View')
