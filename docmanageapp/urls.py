fa3bd72052b37c74c82264b2f661261e8f1411bc
guilhermesurek <guilhermesurek@gmail.com>
2020-03-15 11:46:27 -0300

    Change login route to root

--- docmanageapp/urls.py
+++ docmanageapp/urls.py
@@ -19,6 +19,6 @@ from initial.views import login_view, logout_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login', login_view, name='login'),
+    path('', login_view, name='login'),
     path('logout', logout_view, name='logout'),
 ]
