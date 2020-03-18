d3f2a6d68cbbc69d16e4fe39d9ce4b2ccf6a632e
guilhermesurek <guilhermesurek@gmail.com>
2020-03-18 00:49:29 -0300

    Update settings.py

--- docmanageapp/settings.py
+++ docmanageapp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from docmanageapp.secret import data_base_password, django_secret_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = django_secret_key()
+SECRET_KEY = os.environ['DJANGOSECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [os.environ['docmanageapp'] + '.azurewebsites.net', '127.0.0.1'] if 'docmanageapp' in os.environ else []
