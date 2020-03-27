5f6bf514f9ce7c4290569743c968128ef747dabc
guilhermesurek <guilhermesurek@gmail.com>
2020-03-26 23:09:27 -0300

    update names of fields of upload model

--- upload/models.py
+++ upload/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Document(models.Model):
-    cnpj_sender = models.CharField(max_length=14)
-    cnpj_recipient = models.CharField(max_length=14)
-    document_number = models.CharField(max_length=30)
-    document_key = models.CharField(max_length=44, null=True)
-    document_date = models.DateField()
-    upload_date = models.DateField(auto_now_add=True)
-    doc_file = models.FileField(upload_to='docs/')
+    cnpj_sender = models.CharField("CNPJ emissor", max_length=14)
+    cnpj_recipient = models.CharField("CNPJ destinatário", max_length=14)
+    document_number = models.CharField("Número do documento", max_length=30)
+    document_key = models.CharField("Chave do documento", max_length=44, null=True)
+    document_date = models.DateField("Data de emissão")
+    upload_date = models.DateField("Data de upload", auto_now_add=True)
+    doc_file = models.FileField("Arquivo do documento", upload_to='docs/')
 
     def __str__(self):
         return self.cnpj_sender + '_' + self.document_number + '_' + self.document_date
