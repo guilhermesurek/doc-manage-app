a411357c18c1050b925c29dd11742de404e37192
guilhermesurek <guilhermesurek@gmail.com>
2020-05-05 23:12:02 -0300

    Added search feature in download view and fix js bug in upload view

--- download/views.py
+++ download/views.py
@@ -2,20 +2,36 @@ from django.shortcuts import render
 from django.views import View
 from django.http import JsonResponse
 import math
+from datetime import datetime
 
 from upload.models import Document
 
 class DownloadView(View):
     def get(self, request):
-        docs = Document.objects.all()
         context = {
-            'download_page': 'active',
-            'docs': docs
+            'download_page': 'active'
         }
         return render(request, 'download/download.html', context=context)
 
     def post(self, request):
-        docs = Document.objects.all()
+        request_filter = {}
+        aux = request.POST.get("cnpj_sender")
+        if aux != '':
+            request_filter['sender__cnpj'] = aux
+        aux = request.POST.get("cnpj_recipient")
+        if aux != '':
+            request_filter['recipient__cnpj'] = aux
+        aux = request.POST.get("document_key")
+        if aux != '':
+            request_filter['document_key'] = aux
+        aux = request.POST.get("document_date_min")
+        aux1 = request.POST.get("document_date_max")
+        if aux != '' and aux1 == '':
+            request_filter['document_date'] = aux
+        elif aux != '' and aux1 != '':
+            request_filter['document_date__range'] = [aux, aux1]
+        
+        docs = Document.objects.filter(**request_filter)
         context = {
             'download_page': 'active',
             'docs': docs
